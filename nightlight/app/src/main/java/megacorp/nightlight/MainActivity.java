1091bfe56d39312e0b66489297f027a9dd7f1eb2
wandyezj <wandyezj@outlook.com>
2018-05-02 21:31:04 -0700

    rough draft of blinking background

--- nightlight/app/src/main/java/megacorp/nightlight/MainActivity.java
+++ nightlight/app/src/main/java/megacorp/nightlight/MainActivity.java
@@ -6,19 +6,31 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Switch;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener, Switch.OnClickListener, SensorEventListener {
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener, Switch.OnClickListener, SensorEventListener, GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener {
 
     private SeekBar seekbar_color_selector;
     private TextView textview_selected_color;
     private TextView textview_status;
+    private View view_blink_control;
 
     private Switch switch_color_gyroscope_on;
 
@@ -26,6 +38,10 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
     private Sensor sensor_gyro;
     private Sensor sensor_gravity;
 
+    private GestureDetector  gesture_detector;
+
+    Animation blink_animation;
+    ScheduledThreadPoolExecutor scheduled_executor = new ScheduledThreadPoolExecutor(1);
 
     String color_name[] =
     {
@@ -63,6 +79,9 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         {255, 255, 255} // white
     };
 
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -74,9 +93,10 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         textview_selected_color = (TextView)findViewById(R.id.textview_selected_color);
         textview_status = (TextView)findViewById(R.id.textview_status);
 
-
         switch_color_gyroscope_on = (Switch) findViewById(R.id.switch_color_gyroscope_on);
         switch_color_gyroscope_on.setOnClickListener(this);
+
+        view_blink_control = findViewById(R.id.view_blink_control);
 
         // See https://developer.android.com/guide/topics/sensors/sensors_motion.html
         sensor_manager = (SensorManager) this.getSystemService(Context.SENSOR_SERVICE);
@@ -84,7 +104,84 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         sensor_gravity= sensor_manager.getDefaultSensor(Sensor.TYPE_GRAVITY);
 
         sensor_manager.registerListener(this, sensor_gravity, SensorManager.SENSOR_DELAY_NORMAL);
+
+
+        //View myView = findViewById(R.id.main_view);
+        /*
+        myView.setOnTouchListener(new View .OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+                Log.i("Touch", "onTouch");
+                // ... Respond to touch events
+                return true;
+            }
+        });
+*/
+        gesture_detector = new GestureDetector(MainActivity.this, MainActivity.this);
+
+        //gesture_detector_compact = new GestureDetectorCompat(myView.getContext(), this);
+        gesture_detector.setOnDoubleTapListener(this);
+
+        /*
+        //https://developer.android.com/reference/java/util/concurrent/ScheduledThreadPoolExecutor
+        scheduled_executor.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                blink();
+            }
+        }, 0 , 1000, TimeUnit.MILLISECONDS);
+        */
+
+
+        blink_animation = new AlphaAnimation(0.0f, 1.0f);
+        blink_animation.setDuration(1000); //You can manage the blinking time with this parameter
+        blink_animation.setStartOffset(0);
+        blink_animation.setRepeatMode(Animation.REVERSE);
+        blink_animation.setRepeatCount(Animation.INFINITE);
     }
+
+    boolean is_blinking = false;
+
+
+    private void toggleBlink() {
+
+        is_blinking = !is_blinking;
+
+        if (is_blinking){
+            view_blink_control.setBackgroundColor(current_color);
+            view_blink_control.startAnimation(blink_animation);
+        }else {
+            blink_animation.cancel();
+            view_blink_control.setBackgroundColor(Color.BLACK);
+        }
+
+        blink(is_blinking);
+    }
+
+    private void blink(boolean do_blink) {
+        // TODO: Communicate across bluetooth to the light to set the blink
+    }
+
+    /*
+    boolean blink_on = false;
+    private void blink() {
+        Log.i("Blink", "blink called");
+        if (is_blinking){
+
+            blink_on = !blink_on;
+            if (blink_on)
+            {
+                view_blink_control.setBackgroundColor(current_color);
+            }
+            else
+            {
+                view_blink_control.setBackgroundColor(Color.BLACK);
+            }
+
+        } else {
+            view_blink_control.setBackgroundColor(Color.BLACK);
+        }
+    }
+    */
 
     private void setSelectedColor(int progress_value){
         int[] color_value = color_values[progress_value];
@@ -103,14 +200,21 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
 
     }
 
+    int current_color = Color.BLACK;
+
     private void setSelectedColorTextUi(int r, int g, int b) {
         int progress_value = GetClosestColor(r, g, b);
         textview_status.setText(String.format("Status: %s", progress_value != 0 || gyroscope_on ? "On" : "Off"));
 
         seekbar_color_selector.setProgress(progress_value);
 
+        current_color = Color.rgb(r, g, b);
         textview_selected_color.setText(color_name[progress_value]);
-        textview_selected_color.setTextColor(Color.rgb(r, g, b));
+        textview_selected_color.setTextColor(current_color);
+
+        if (is_blinking) {
+            view_blink_control.setBackgroundColor(current_color);
+        }
     }
 
 
@@ -232,5 +336,66 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
 
+    }
+
+    //
+    // Tap listeners - handle touch events
+    //
+
+    // http://codetheory.in/android-gesturedetector/
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        gesture_detector.onTouchEvent(event);
+
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent motionEvent) {
+        Log.i("Touch", "Single Tap");
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent motionEvent) {
+        Log.i("Touch", "Double Tap");
+        toggleBlink();
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent motionEvent) {
+        Log.i("Touch", "Double Tap Event");
+        return false;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        return false;
     }
 }
